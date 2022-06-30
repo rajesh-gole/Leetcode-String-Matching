273cc70a761182d795fd061161ca3be45eb80c30
RAJESH GOLE <44594864+rajesh-gole@users.noreply.github.com>
2022-06-30 17:31:41 +0530

    Time: 13 ms (30.82%), Space: 8.3 MB (71.84%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -1,18 +1,32 @@
 class Solution {
+    
+// We need this function to define how to sort
+// the vector. We will pass this function into the
+// third parameter and it will tell it to sort descendingly.
+    
+  static bool wayToSort(const string &a, const string &b)
+    {
+        return a.size() < b.size();
+    }
 public:
     vector<string> stringMatching(vector<string>& words) {
+        
+ // Do not include the () when you call wayToSort
+// It must be passed as a function pointer or function object
+        
+        sort(words.begin(), words.end(), wayToSort);
         vector<string> res;
-            for(auto st:words)
+        for(int i = 0; i < words.size(); i++)
+        {
+            for(int j = i + 1; j < words.size(); j++)
             {
-                    for(auto wo:words)
-                    {
-                            if(st.length()< wo.length() && wo.find(st)!=string::npos)
-                            {
-                                   res.push_back(st);
-                                    break;
-                            }
-                    }
+                if(words[j].find(words[i]) != -1)
+                {
+                    res.push_back(words[i]);
+                    break;
+                }
             }
-            return res;
+        }
+        return res;
     }
 };
